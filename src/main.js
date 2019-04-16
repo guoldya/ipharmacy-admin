823d542664943afd8b9c204aeac57e6cf817a9fc
唐向光 <185367128@qq.com>
2019-04-16 16:38:00 +0800

    入库点文件  引用整理

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+
+//核心引用
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -5,40 +7,37 @@ import store from './store/'
 import config from '@/config'
 import axios from 'axios'
 import notification from 'ant-design-vue/es/notification'
-//引入Element的DataTable
-import { Table, TableColumn,Dialog,Card,Tag,Collapse,CollapseItem } from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 
-// polyfill
-import 'babel-polyfill'
-// 核心插件
+//功能相关插件  可自定义卸载
 import admin from '@/plugin/admin'
-
-import { VueAxios } from '@/utils/request' // axios 不建议引入到 Vue 原型链上
-import $ from 'jquery'
-// flex 布局库
-import 'flex.css'
-window.$ = $
-
-//引入自定义样式
-import '@/style/main.css'
-
-import './core/use'
 import bootstrap from './core/bootstrap'
-import '@/permission' // permission control
-import '@/utils/filter' // global filter
+import { VueAxios } from '@/utils/request' 
+import './core/use'
+import '@/permission' // vue 权限控制
+import '@/utils/filter' // vue 全局 filter  
+import 'babel-polyfill' // polyfill
 
-// [ 可选组件 ] 网格布局
-import { GridLayout, GridItem } from 'vue-grid-layout'
-
-
-import msgHandler from '@/comm-msg/msg-handler.js'
-import Enum from '@/comm-data/enum.js'
-import Searchpanel from '@/components/search-panel'
-import { getFormat } from '@/utils/misc'
-import util from '@utils/util'
-import {asyncRouterMap, constantRouterMap} from '@/config/router.config'
+//组件相关
+import { Table, TableColumn,Card,Tag,Collapse,CollapseItem } from 'element-ui'
+import { GridLayout, GridItem } from 'vue-grid-layout' // [ 可选组件 ] 网格布局
 import vcolorpicker from 'vcolorpicker'
+
+//业务相关组件
+import msgHandler from '@/comm-msg/msg-handler.js'
+import Searchpanel from '@/components/search-panel'
+import util from '@utils/util'
+import { getFormat } from '@/utils/misc'
+import {asyncRouterMap} from '@/config/router.config'
+import Enum from '@/comm-data/enum.js'
+
+//样式导入
+import 'element-ui/lib/theme-chalk/index.css'
+import 'flex.css'// flex 布局库
+import '@/style/main.css'//引入自定义样式
+
+
+
+
 
 // 核心插件
 Vue.use(admin)
@@ -49,19 +48,16 @@ Vue.use(Tag)
 Vue.use(Collapse)
 Vue.use(CollapseItem)
 Vue.use(vcolorpicker)
-//统一错误处理
-Vue.use(msgHandler)
+Vue.use(msgHandler)//统一错误处理
+Vue.use(VueAxios, router)
+
 Vue.config.productionTip = false
 // 当前环境
 Vue.prototype.$env = process.env.NODE_ENV
-
-Vue.use(VueAxios, router)
-//引入Axios
-Vue.prototype.$axios = axios
+Vue.prototype.$axios = axios //引入Axios
 Vue.prototype.getFormat = getFormat
 Vue.prototype.$config = config
-//公用枚举源
-Vue.prototype.enum = Enum
+Vue.prototype.enum = Enum//公用枚举源
 Vue.prototype.$debuger = util.debuger
 Vue.prototype.$notification = notification
 
