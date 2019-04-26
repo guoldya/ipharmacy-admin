bf49e0050a3617822a105425db66d7970fe06e65
唐向光 <185367128@qq.com>
2019-04-26 17:49:40 +0800

    1

---
+++ src/views/flowChartEditor/nodemode/model-node.js
@@ -0,0 +1,99 @@
+import G6Editor from '@antv/g6-editor';
+import G6 from "@antv/g6";
+const { Flow } = G6Editor;
+
+Flow.registerNode("model-node", {
+    draw: function(t) {
+        var e = t.getModel(),
+        n = t.getGraphicGroup(),
+        i = this.getNodeSize(e),
+        o = this.getStyle(t),
+        u = function(t, e, n, i) {
+            return e.addShape("path", {
+                attrs: r({},
+                i, {
+                    path: n
+                })
+            })
+        } (0, n, this.getPath(i, o), o);
+        return e.label &&
+        function(t, e) {
+            var n = a.mix(!0, {},
+            s.nodeLabelStyle, {
+                x: 0,
+                y: 0
+            });
+            a.isString(t) ? n.text = t: a.mix(n, t),
+            e.addShape("text", {
+                attrs: n
+            })
+        } (e.label, n),
+        u
+    },
+    getNodeSize:function(e)
+    {
+        let t = e.size;
+        return t.split('*');
+    },
+    getStyle: function(t) {
+        var e, n, r = t.getModel(),
+        i = this.getColor(t);
+        // if (i) {
+        //     var o = G6.Util.Palettes.generate(i);
+        //     e = o[0],
+        //     n = o[3]
+        // }
+        return  {
+            fill: i,
+            stroke: i
+        };
+    },
+    color: null,
+    getColor: function(t) {
+        var e = t.getModel();
+        return e.color ? e.color: this.color
+    },
+    getPath: function(t, e) {
+        return a.getRectPath( - t[0] / 2, -t[1] / 2, t[0], t[1], e.radius)
+    },
+    getActivedOutterStyle: function() {
+        return s.nodeActivedOutterStyle
+    },
+    getActivedStyle: function(t) {
+        var e = this.getColor(t);
+        if (e) {
+            var n = a.Palettes.generate(e);
+            return r({},
+            s.nodeActivedStyle, {
+                fill: n[0],
+                stroke: n[5]
+            })
+        }
+        return s.nodeActivedStyle
+    },
+    getSelectedStyle: function(t) {
+        var e = this.getColor(t);
+        if (e) {
+            var n = a.Palettes.generate(e);
+            return r({},
+            s.nodeSelectedStyle, {
+                fill: n[2],
+                stroke: n[5]
+            })
+        }
+        return s.nodeSelectedStyle
+    },
+    getSelectedOutterStyle: function(t) {
+        var e = this.getColor(t);
+        if (e) {
+            var n = a.Palettes.generate(e);
+            return r({},
+            s.nodeSelectedOutterStyle, {
+                stroke: n[1],
+                fill: n[1]
+            })
+        }
+        return s.nodeSelectedOutterStyle
+    },
+    anchor: [[.5, 0], [1, .5], [.5, 1], [0, .5]]
+})
---
+++ src/views/flowChartEditor/nodemode/model-rhombus.js
@@ -0,0 +1,91 @@
+import G6Editor from '@antv/g6-editor';
+import G6 from "@antv/g6";
+const { Flow } = G6Editor;
+
+Flow.registerNode("model-rhombus", {
+    draw: function (item) {
+        let group = item.getGraphicGroup();
+        let model = item.getModel();
+        let shapeObj = item.getShapeObj();
+
+        let label = model.label != null ? model.label : shapeObj.label;
+
+        var i = model.size.split('*');
+
+        const x = -i[0] / 2
+        const keyShape = group.addShape('path', {
+            attrs: {
+                path: this.getPath(i),
+                fill: this.getColor(item)
+            }
+        })
+
+        let index = 0
+        let arrayText = this.addShapePnewline(label);
+        let labely = this.getlabelY(label, arrayText);
+        for (let i = 0; i < arrayText.length && i < 2; i++) {
+            this.addShapeP(group, x + 20, labely + index, arrayText[i])
+            index += 20
+        }
+        return keyShape;
+    },
+    getlabelY: function (label, arrayText) {
+        let arrayTextlength = arrayText.length > 2 ? 2 : arrayText.length;
+        let yy = -(((arrayTextlength - 1) * 20)) / 2 - 5;
+        console.log(yy);
+        return yy;
+    },
+    addShapeP: function (group, xx, yy, text) {
+        const fontPStyle = {
+            attrs: {
+                text: text,
+                x: 0,
+                y: yy,
+                textAlign: 'center',
+                fontFamily: '微软雅黑',
+                fontSize: 12,
+                textBaseline: 'top',
+                fill: 'rgba(0,0,0,0.65)'
+            }
+        }
+        group.addShape('text', fontPStyle)
+    },
+    addShapePnewline: function (text) {
+        text = text
+            .replace(/[^\x00-\xff]/g, '$&\x01')
+            .replace(/.{20}\x01?/g, '$&\n')
+            .replace(/\x01/g, '')
+        return text.split('\n')
+
+    },
+    getColor: function (t) {
+        var e = t.getModel();
+        return e.color ? e.color : this.color
+    },
+    //获取绘制路线 塞贝尔曲线
+    getPath: function (t) {
+        var e = t[0],
+            n = t[1],
+            r = [{
+                x: 0,
+                y: 0 - n / 2
+            },
+            {
+                x: 0 + e / 2,
+                y: 0
+            },
+            {
+                x: 0,
+                y: 0 + n / 2
+            },
+            {
+                x: 0 - e / 2,
+                y: 0
+            },
+            {
+                x: 0,
+                y: 0 - n / 2
+            }];
+        return G6.Util.pointsToPolygon(r)
+    }
+});
