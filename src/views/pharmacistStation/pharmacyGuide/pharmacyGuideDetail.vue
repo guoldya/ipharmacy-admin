e0f1ec946fd0c01b3fb9347f034b79ed3492fbc2
SweetGui <1589128729@qq.com>
2019-08-06 18:04:11 +0800

    1、用药指导与用药指导详情界面优化

--- src/views/pharmacistStation/pharmacyGuide/pharmacyGuideDetail.vue
+++ src/views/pharmacistStation/pharmacyGuide/pharmacyGuideDetail.vue
@@ -18,7 +18,21 @@
                 </a-row>
                 <a-row>
                     <a-col>
+                      <el-table
+                        :header-cell-style="headerCellStyle"
+                        highlight-current-row
+                        class="margin-top-10"
+                        :data="date" border
+                      >
+                        <el-table-column
+                          :show-overflow-tooltip="true"
+                          :key="1"
+                          prop="value"
+                          align="center"
 
+                        >
+                        </el-table-column>
+                      </el-table>
                     </a-col>
                 </a-row>
               </a-card>
@@ -53,7 +67,7 @@
               <div class="medicineGuide">
                 <span><a-icon type="medicine-box" /> 指导信息</span>
                 <span>
-                  <a-button @click=""><a-icon type="arrow-left" />返回</a-button>
+                  <a-button @click="backTo"><a-icon type="arrow-left" />返回</a-button>
                   <a-button type="primary">新增</a-button>
                   <a-button type="primary">保存</a-button>
               </span>
@@ -71,8 +85,8 @@
                 </a-col>
 
                 <a-col :span="18">
-                  <a-input placeholder="large size" style="width: 200px;margin-left: 12px"/>
-                  <a-input placeholder="large size" style="width: 200px;margin-left: 12px" />
+                  <a-input placeholder="范华" style="width: 200px;margin-left: 12px"/>
+                  <a-input placeholder="15245647896" style="width: 200px;margin-left: 12px" />
                 </a-col>
               </a-row>
 
@@ -90,11 +104,11 @@
               <a-row class="margin-top-10">
                 <a-col :span="8">
                   记录时间：
-                  <a-input placeholder="large size" style="width: 200px;margin-left: 24px" />
+                  <a-input placeholder="2019-06-30" style="width: 200px;margin-left: 24px" />
                 </a-col>
                 <a-col :span="16">
                   有效时间：
-                  <a-input placeholder="large size" style="width: 200px;margin-left: 24px" />
+                  <a-input placeholder="2019-07-30" style="width: 200px;margin-left: 24px" />
                 </a-col>
               </a-row>
 
@@ -109,18 +123,27 @@
             <div class="plusBtn margin-top-10"><a-button type="primary"><a-icon type="plus" />问题医嘱</a-button></div>
 
             <a-spin :spinning="loading" tip="加载中...">
-              <a-table
+              <el-table
+                highlight-current-row
                 class="margin-top-10"
-                :columns="doctorAdviceContentColumns"
                 :data="doctorAdviceContentData" border
-                :highlight-current-row="true"
               >
+                <el-table-column
+                  v-for="item in doctorAdviceContentColumns"
+                  :show-overflow-tooltip="true"
+                  :key="item.value"
+                  :label="item.title"
+                  :prop="item.value"
+                  :width="item.width"
+                  :align="item.align"
+                >
+                </el-table-column>
                 <el-table-column fixed="right" label="操作" :width="100" align="center" v-if="true">
                   <template slot-scope="scope">
                    <span>删除</span>
                   </template>
                 </el-table-column>
-              </a-table>
+              </el-table>
             </a-spin>
 
             <div class="disFlex margin-top-10">
@@ -129,18 +152,27 @@
             </div>
 
             <a-spin :spinning="loading" tip="加载中...">
-              <a-table
+              <el-table
+                highlight-current-row
                 class="margin-top-10"
-                :columns="doctorAdviceInfoColumns"
-                :data="doctorAdviceInfoData" border
-                :highlight-current-row="true"
+                :data="doctorAdviceContentData" border
               >
+                  <el-table-column
+                    v-for="item in doctorAdviceInfoColumns"
+                    :show-overflow-tooltip="true"
+                    :key="item.value"
+                    :label="item.title"
+                    :prop="item.value"
+                    :width="item.width"
+                    :align="item.align"
+                  >
+                  </el-table-column>
                 <el-table-column fixed="right" label="操作" :width="100" align="center" v-if="true">
                   <template slot-scope="scope">
                     <span>删除</span>
                   </template>
                 </el-table-column>
-              </a-table>
+              </el-table>
             </a-spin>
 
             <div class="disFlex margin-top-10">
@@ -166,25 +198,40 @@
     name: 'pharmacyGuideDetail',
     data(){
       return{
+        date:[
+          {value:'2019-08-01'},
+          {value:'2019-08-02',},
+          {value:'2019-08-03'},
+          {value:'2019-08-04'},
+          {value:'2019-08-05'},
+          {value:'2019-08-06'},
+          {value:'2019-08-07'},
+
+        ],
         loading:false,
         data:[{time:'2010-01-01'},{time:'2011-01-01'},{time:'2012-01-01'}],
         doctorAdviceContentData:[],
-        plainOptions:['用法用量','方案疗程','给药途径','联合用药'],
-        checkValue:[],
+        plainOptions:[
+          { label: '用法用量', value: '1' },
+          { label: '方案疗程', value: '2' },
+          { label: '给药途径', value: '3' },
+          { label: '联合用药', value: '4' },
+        ],
+        checkValue:['1'],
         doctorAdviceContentColumns:[
-          { title: '医嘱类容', dataIndex: 'planName', width: 250 },
-          { title: '问题类别', dataIndex: 'planType', align: 'center', width: 100,format:this.planType },
-          { title: '问题描述', dataIndex: 'planScope', align: 'center', width: 100,format:this.planScope },
-          { title: '创建时间', dataIndex: 'describe' },
+          { title: '医嘱类容', dataIndex: 'planName', width: 250,align: 'left' },
+          { title: '问题类别', dataIndex: 'planType', align: 'left', width: 100,format:this.planType },
+          { title: '问题描述', dataIndex: 'planScope', align: 'left', width: 100,format:this.planScope },
+          { title: '创建时间', dataIndex: 'describe' , align: 'left'},
         ],
         doctorAdviceInfoColumns:[
-          { title: '医嘱类型', dataIndex: 'planName', width: 250 },
-          { title: '类别', dataIndex: 'planType', align: 'center', width: 100,format:this.planType },
-          { title: '频次', dataIndex: 'planScope1', align: 'center', width: 100,format:this.planScope },
-          { title: '频次', dataIndex: 'planScope2', align: 'center', width: 100,format:this.planScope },
-          { title: '剂量', dataIndex: 'describe' },
-          { title: '用法', dataIndex: 'planScope3', align: 'center', width: 100,format:this.planScope },
-          { title: '备注', dataIndex: 'planScope4', align: 'center', width: 100,format:this.planScope },
+          { title: '医嘱类型', dataIndex: 'planName', width: 250 , align: 'left'},
+          { title: '类别', dataIndex: 'planType', align: 'left', width: 100,format:this.planType },
+          { title: '医嘱项目名称', dataIndex: 'planScope1', align: 'left', width: 100,format:this.planScope },
+          { title: '频次', dataIndex: 'planScope2', align: 'left', width: 100,format:this.planScope },
+          { title: '剂量', dataIndex: 'describe' , align: 'left'},
+          { title: '用法', dataIndex: 'planScope3', align: 'left', width: 100,format:this.planScope },
+          { title: '备注', dataIndex: 'planScope4', align: 'left', width: 100,format:this.planScope },
         ],
         doctorAdviceInfoData:[],
         medicineAdviceInfoColumns:[],
@@ -196,6 +243,15 @@
         console.log('checked = ', checkedValues)
         console.log('value = ', this.checkValue)
       },
+      headerCellStyle({row, rowIndex}){
+        return 'display:none'
+      },
+      backTo(){
+        this.$router.push({
+          name: 'index',
+          // path:'index'
+        })
+      }
     }
   }
 </script>
